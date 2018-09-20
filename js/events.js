f82a9e90499e1e3c950be0695a7538d5bf53b0e5
Matteo Valles <matteovalles@gmail.com>
2018-09-20 21:39:52 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+    return
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+    return
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert('you pressed G')
+      return
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.")
+    return
+  })
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
+submitIt();
 
 });
